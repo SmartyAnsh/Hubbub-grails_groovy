1a514565c44a57ba094576fed0961c0e38636026
Anshul Bansal <smartdiscover17@gmail.com>
2013-11-06 13:44:33 +0530

    Create UserData.groovy
    This domain class contains unique userId, password, role(user/admin), securityQues, securityAns, homepage, dateCreated
    profile as ProfileOfUser object.
    This class is mapped with one-to-many mapping with PostOfUser,
    TaggingOfUser and UserData itself(for following).

---
+++ UserData.groovy
@@ -0,0 +1,33 @@
+
+class UserData {
+	String userId
+	String password
+	Role role
+	SecurityQuestion securityQues
+	String securityAns
+	String homepage
+	Date dateCreated
+	ProfileOfUser profile
+	
+
+    static constraints = {
+		userId(size:3..20,unique:true)
+		password(size:6..8,validator:{passwd,user -> passwd !=user.userId})
+		homepage(url:true,nullable:true)
+		profile(nullable:true)
+		role(nullable:true)
+		securityQues(blank:false,nullable:false)
+		securityAns(blank:false,nullable:false)
+    }
+	
+	static mapping = {
+		profile lazy:false
+		posts sort :'dateCreated'
+	}
+	
+	static hasMany = [posts:PostOfUser,tags:TaggingOfUser,following:UserData]
+	
+	String toString() {
+		"${userId}"
+		};
+}
